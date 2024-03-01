7c9270924ed3795c4a52d937dada814e5c63102b
Documenter.jl <documenter@juliadocs.github.io>
2024-03-01 13:10:01 +0000

    build based on cc9c0f7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NicamFiles","category":"page"},{"location":"#NicamFiles","page":"Home","title":"NicamFiles","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NicamFiles.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NicamFiles]","category":"page"}]
+}
